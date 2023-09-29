68ac2c3964881ae734f024759ae8b94746ff23f1
MayankJadav007 <146413657+MayankJadav007@users.noreply.github.com>
2023-09-29 11:21:50 +0530

    Add files via upload

---
+++ other.js
@@ -0,0 +1,1189 @@
+//  var myName ='mayank jadav';
+ var make = 6+545+45 ;
+//  console.log(make);
+
+//  console.log(typeof(myName));
+// console.log(typeof(make));
+
+// console.log("10" - 20);
+
+// console.log(NaN===NaN);
+
+// console.log(5+10);
+
+var num = 15;
+// var newNum = ++num + 5;
+var newNum = ++num +5 ;
+
+
+// console.log(num);
+// console.log(newNum);
+
+let x = 100;
+x <<= 5;
+// console.log(x);
+
+
+var a = 10
+var b = 20
+a = a+b
+b = a-b
+a = a-b
+// console.log(a);
+// console.log(b);
+
+// let age = 15;
+// let firstName = "mayank";
+
+// age = age + "";
+// console.log(age);
+// console.log(typeof(age));
+
+// let mystr = +"35";
+
+// console.log(mystr);
+// console.log(typeof(mystr));
+
+// let string1 = "mayank";
+// let string2 = "nisha";
+// let fullName = string1 + " " + string2;
+
+// console.log(fullName);
+// let string3 = "10";
+// let string4 = "25";
+
+// let newstring = +string3 +  +string4 ;
+// console.log(newstring);
+// console.log(typeof(newstring));
+ 
+// let age = "22";
+// let firstName = "mayank"
+
+// let aboutme= `my name is ${firstName} and my age ${age}`; 
+// console.log(aboutme);
+
+
+// let firstName;
+// console.log(typeof firstName);
+// firstName="mayank";
+// console.log(typeof firstName, firstName);
+// console.log(typeof null);
+
+// let myName = "mayank jadav";
+// console.log(myName.length);
+
+
+// let any= `we are the mayank "jadav" are the value`;
+// console.log(any);
+
+// const myBiodata = "I am   the jadav mayank tecnial";
+// console.log(myBiodata.indexOf("t", 4));
+// console.log(myBiodata.length);
+
+// console.log(myBiodata.lastIndexOf("t", 9));
+
+var str = "apple, banana, kiwi";
+// let res= str.slice(7,9);
+// console.log(res);
+
+// let res = str.substring(8,-2);
+// let res = str.substr(-10);
+// let res1 = str.substr(8,-2);
+
+// console.log(res);
+// console.log(res1);
+
+
+// let myBiodata = `i am mayank jadav bahadur mayank `
+// console.log(myBiodata.replace('mayank','Mayank'));
+// console.log(myBiodata.replace)
+// let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// console.log(text.length);
+
+// let text = "Apple, Banana, Kiwi";
+// console.log(text.substr(6,4));
+
+// let text = "Please visit Microsoft!";
+// console.log(text.replace('Microsoft', 'w3school'));
+// let text = "Please visit Microsoft and Microsoft!";
+// console.log(text.replace(/Microsoft/g,"mayank"));
+
+// let text = "Hello World!";
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+
+
+// let text1 = "Hello123";
+// let text2 = "World123";
+// console.log(text1.concat(" ",text2));
+
+
+// text = "Hello" + " " + "World!";
+// console.log("Hello".concat(" ","World mayank "));
+
+// let text1 = "      Hello World!      ";
+// console.log(text1.trim());
+// console.log(text1.trimStart());
+// console.log(text1.trimEnd());
+//
+// let text = 10;
+// console.log(text.padStart(4,9));
+// console.log(text.padEnd(4,9));
+
+// let text1 = text.toString();
+// console.log(text1.padEnd(4, "2"));
+
+// let text = "HELLO WORLD";
+// console.log(text.charAt(1,9));
+
+
+// let text = "Please locate where 'locate' occurs!";
+// console.log(text.indexOf("locate"));
+
+// console.log(text.lastIndexOf("locate"));
+// console.log(text.lastIndexOf("mayank"));
+// console.log(text.lastIndexOf("locate",7));
+// console.log(text.indexOf("locate",15));
+
+
+
+// let text = "Please locate where 'locate' occurs!";
+// console.log(text.search(/where/));
+
+// let text = "The rain in SPAIN stays mainly in the plain";
+// const myArr = text.match(/ain/);
+// console.log(myArr.length + " " + myArr);
+
+// console.log(text.match("ain"));
+// console.log(text.match(/ain/));
+
+// let text = "I love cats. Cats are very easy to love. Cats are very popular."
+// console.log(text.match("cats"));
+// console.log(text.matchAll(text.length + " " + "cats"));
+
+// console.log(text.includes('love'));
+
+
+// let text = "world, welcome Hello to the universe.";
+// let text = "Hello world, welcome to the universe. universe.";
+
+// console.log(text.includes("Hello",9));
+
+// console.log(text.startsWith("world",0));
+// console.log(text.startsWith("world"));
+
+// console.log(text.endsWith(" universe."));
+
+
+// let text = "The rain in SPAIN stays mainly in mayank the plain";
+// const myArr = text.match("ain");
+
+//  console.log(text.match( myArr.length + " " + myArr));
+
+//  console.log(text.match(text.length + " " + "mayank"));
+
+
+
+// let text = `Hellovsklnsngngkgioje world!`;
+// console.log(text);
+  
+// let text1 = "mayank jadav";
+// console.log(text1);
+
+// let text2 = `he's mayank "jadav"`;
+// console.log(text2);
+
+// let text =
+// `The quickeevmvoplbmv cs  csov c vkpl c kcsv v k cs
+// "brown fox
+// jumps over
+// the lazy dog"`;
+
+// console.log(text);
+
+// let firstName = "mayank";
+// let lastName = "jadav";
+
+// console.log(`welcome ${firstName}, ${lastName}`);
+
+
+
+// let text = "jadav mayank mayank The rain in SPAIN stays mainly in the plain";
+// console.log(text.match("ain/g"));
+// console.log(text.matchAll("mayank"));
+// console.log(text.match("mayank"));
+
+
+// let price = 10;
+// let VAT = 0.25;
+
+// let total = `Total: ${(price * (1 + VAT)).toFixed(0)}`;
+// console.log(total);
+
+// let firstName = "John";
+// let lastName = "Doe";
+// console.log(`welcome ${firstName}, ${lastName}`);
+
+// =====================================================================
+// -================================================================
+// =============                 =Array==                ===============================
+
+// var myfriend = ['mayank', 'jadav', 'nayan', 'nikul', 'vaibhav'];
+// console.log(myfriend[3]);
+// console.log(myfriend.length);
+
+
+// console.log(myfriend[myfriend.length -3]);
+
+// const fruits = ["banana", "apple", "kiwi","Mango"]; 
+// console.log(fruits.toString());
+// console.log(fruits.push("mayank"));
+
+// const myArr = [ 0, 1, 2, 3, 4, 5];
+// const myHero = ["saktiman", "airman", "Thor"];
+
+// const myArr1 = new Array(1, 2, 3, 4,);
+// console.log(myArr[1]);
+
+// console.log(myArr);
+// myArr.push(8);
+// myArr.pop();
+// myArr.shift();
+// myArr.unshift(200);
+// myArr.splice(1,4);
+// myArr.reverse();
+
+// console.log(myArr);
+// 
+
+// const points = new Array(40,12);
+// const point =[40, 100, 1, 5, 25, 10];
+
+// console.log(point[40]);
+// console.log(points,"1");
+// console.log(points);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango",25];
+// const myGirls = ["Cecilie", "Lone", "love", "keval"];
+
+// console.log(typeof fruits);
+// console.log(Array.isArray(fruits));
+// fruits[fruits.length]= "mayank";
+// console.log(fruits);
+
+// fruits.push(2);
+// console.log(fruits);
+
+// const my =  fruits.concat(myGirls);
+// const my1 = myGirls.concat("nisha");
+// console.log(my1);    
+
+// const myArr = [[1,2],[3,4],[5,6]];
+// // console.log(myArr.flat());
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango",25];
+// const myGirls = ["Cecilie", "Lone", "love", "keval"];
+
+
+// fruits.splice(2,"montu", "komal");
+// fruits.slice(1);
+// console.log(fruits);
+  
+
+// const myArr = [ 0, 1, 2, 3, 4, 5]; 
+
+// const myArr1 = myArr.slice(0,5);
+// const myArr2 = myArr.splice(1,3);
+
+// console.log(myArr1);
+// console.log(myArr2);
+
+// console.log(Array.isArray("mayank"));
+
+// console.log(Array.from("Mayank"));
+
+// let mayank1 =100;
+// let mayank2 =200;
+// let mayank3 =300;
+
+// console.log(Array.of(mayank1,mayank2,mayank3));
+
+// const months = ['March', 'Jan', 'Feb', 'Dec'];
+// months.sort();
+// console.log(months);
+
+
+// const array1 = [1, 30, 4, 21, 100000,1111];
+// array1.sort();cls 
+
+// console.log(array1);
+
+
+// const months = ['March', 'Jan', 'Feb', 'Dec'];
+// months.reverse();
+// console.log(months);
+
+
+// const point =[ 40, 100, 1, 5, 25, 10];
+// console.log(point);
+
+// point.sort(function(a,b){
+//     return(b-a)
+// });
+// console.log(point);
+// //  ===================================================
+// point.sort(function(a,b){
+//     return(a-b)
+// });
+// console.log(point);
+
+
+
+// function myFunction(g1, g2) {
+//     return g1 / g2;
+// }
+// const value = myFunction(8, 2); 
+// console.log(value);
+
+
+const point =[ 40, 100, 1, 5, 25, 10];
+point.sort(function(){
+    return 0.5 - Math.random()
+});
+console.log(point);
+
+var as = ["max","jack","sam"];  
+var s   = as.sort(func);
+
+function func(a,b){
+     return Math.random();
+}
+console.log(s);
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
